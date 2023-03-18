350f9f4a394847acf03235f0197276a2645a4c63
prachi91124 <prachianand321@gmail.com>
2023-03-18 20:04:16 +0530

    Write python file is created

---
+++ write.py
@@ -0,0 +1,26 @@
+def over_write(List, Dictionary):   # an overwrite function
+    L = List    # assign list with variable name 'L'
+    d = Dictionary    # assign Dictionary with variable name 'd'
+    """
+    Update quantity of product after customer purchased some quantity of any product.
+    And print remaining stock products.
+    """
+    for keys in d.keys():
+        if keys == "chips":
+            L[0][4] = str(int(L[0][4])-d['chips'])
+        elif keys == "water bottle":
+            L[1][4] = str(int(L[1][4])-d['water bottle'])
+        elif keys == "milk":
+            L[2][4] = str(int(L[2][4])-d['milk'])
+        elif keys == "curd":
+            L[3][4] = str(int(L[3][4])-d['curd'])
+        else:
+            L[4][4] = str(int(L[4][4])-d['bread'])
+    print("\nRemaining Stock Products:\n",L)
+        
+    files = open("products.txt", "w")  # opens stock file (products.txt) file in write mode.
+    for each in L:
+        files.write(str(",".join(each)))
+        files.write("\n")         
+    files.close()
+    return
